2a1bbf5e536c548f66866901b2d1fffdeb525ad1
iannxz <iannarthur90@gmail.com>
2025-09-19 00:52:57 -0300

    exerc3

--- server.js
+++ server.js
@@ -1,43 +1,40 @@
-// Configuração do sistema
 const express = require("express");
-const fs = require("fs/promises");
 const app = express();
-const PORT = 2600;
-
-app.get("/calculadora", (req, res) => {
-  const { operacao, n1, n2 } = req.query;
-
-  if (!operacao || !n1 || !n2) {
-    return res.status(400).json({ error: "parametros insuficientes" });
-  }
-
-  if (isNaN(n1) || isNaN(n2)) {
-    return res.status(400).json({ error: "n1 e n2 devem ser números" });
-  }
-
+const PORT = 2601;
+ 
+app.get("/operacao/:tipo", (req, res) => {
+  const { tipo } = req.params;
+  const { n1, n2 } = req.query;
+ 
+ 
   let resultado;
-  switch (operacao) {
+ 
+  switch (tipo) {
     case "soma":
       resultado = Number(n1) + Number(n2);
       break;
+ 
     case "subtracao":
       resultado = Number(n1) - Number(n2);
       break;
+ 
     case "multiplicacao":
       resultado = Number(n1) * Number(n2);
       break;
+ 
     case "divisao":
       resultado = Number(n1) / Number(n2);
       break;
-  }
-  
-  res.json({ resultado });
+    }
+ 
+ 
+  res.send(`resultado da ${tipo} é ${resultado}`);
 });
-
+ 
 app.use((req, res) => {
   res.status(404).json({ message: "pagina nao encontrada" });
 });
-
+ 
 app.listen(PORT, () => {
-  console.log(`Servidor ativo na porta ${PORT}`);
+  console.log(`Servidor Ativo na porta ${PORT}`);
 });
